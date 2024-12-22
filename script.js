0dbadbbaa6e915a03c915e8ddb3f79ebe9138cd6
jlamaral <159718926+JLAmaral@users.noreply.github.com>
2024-12-22 10:20:19 -0500

    restructured the index page base css set operate functions all work

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-const numButton = document.querySelectorAll('.number');
-const clearButton = docuent.querySelector('.clear');
+const calcInput = document.querySelector('.calc-input') 
+const allButtons = document.querySelectorAll('.button')
 
 
+allButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.dataset.value
 
-// calculator will contain functions for all the basic math operators. start with:
-function add(num1, num2) {
-    return num1 + num2;
-};
+        if (value === '=') {
+            if (calcInput.innerText.includes('/')) {
+                const splitStuff = calcInput.innerText.split('/')
+                operate('/', splitStuff[0], splitStuff[1])
+            }
 
-function sub(num1, num2) {
-    return num1 - num2;
-};
+            if (calcInput.innerText.includes('*')) {
+                const splitStuff = calcInput.innerText.split('*')
+                operate('*', splitStuff[0], splitStuff[1])
+            }
 
-function mult(num1, num2) {
-    return num1 * num2;
-};
+   
+            if (calcInput.innerText.includes('-')) {
+                const splitStuff = calcInput.innerText.split('-')
+                operate('-', splitStuff[0], splitStuff[1])
+            }
 
-function div(num1, num2) {
-    return num1 / num2;
-};
+            if (calcInput.innerText.includes('+')) {
+                const splitStuff = calcInput.innerText.split('+')
+                operate('+', splitStuff[0], splitStuff[1])
+            }
+        }
 
-// Create 'operate' function that takes an operator and two numbers and then calls one of the above functions on the numbers.
+        calcInput.innerText += value
 
-function operate(num1, num2, operator) {
-    switch (operator){
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return sub(num1, num2);
-        case '*':
-            return mult(num1, num2) 
-        case '/': 
-            return div(num1, num2);
+        if (value === 'clear') {
+            calcInput.innerText = ''
+        }
+    })
+})
+
+const operate = (operator, value1, value2) => {
+    const num1 = Number(value1)
+    const num2 = Number(value2)
+
+    if (operator === '/') {
+        calcInput.innerText = num1 / num2
     }
-};
 
-//Create a basic HTML calculator with buttons for each digit and operator (including =).
+    if (operator === '*') {
+        calcInput.innerText = num1 * num2
+    }
 
+    if (operator === '-') {
+        calcInput.innerText = num1 - num2
+    }
 
-
-
-
-
-
-
-
-
-// make calculator buttons, create buttons with javascript instead of tons of divs if possible
+    if (operator === '+') {
+        calcInput.innerText = num1 + num2
+    }
+}
